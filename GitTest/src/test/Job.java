d595a2e9dcfeeb2357d991559128cbf2edffbb18
jinyoung kim <rlaw2992@naver.com>
2022-06-23 19:55:01 +0900

    test21

--- GitTest/src/test/Job.java
+++ GitTest/src/test/Job.java
@@ -3,6 +3,8 @@ public interface Job {
 	public void work(); 
 	public default void getMoney(int money) {
 		System.out.println(money + "À» ¹ÞÀ½");
+		System.out.println("hi");
+		System.out.println("hi");
 	}
 
 }
