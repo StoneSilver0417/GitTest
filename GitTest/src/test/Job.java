6fe43fde44717b7174a5e1484ccaba6346e78074
Woosek <waterdrop11@naver.com>
2022-06-24 11:45:29 +0900

    한글수정2

--- GitTest/src/test/Job.java
+++ GitTest/src/test/Job.java
@@ -1,8 +1,13 @@
+package test;
+
 
 public interface Job {
 	public void work(); 
 	public default void getMoney(int money) {
+		
+	}
+	public static void main(String[] args) {
+		System.out.println("한글이 깨져요!");
 		System.out.println("한글이 깨져요!");
 	}
-
 }
