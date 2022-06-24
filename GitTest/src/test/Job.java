d764fe87016850e669dd396e4600deb81442acca
Woosek <waterdrop11@naver.com>
2022-06-24 11:16:15 +0900

    한글깨짐확인

--- GitTest/src/test/Job.java
+++ GitTest/src/test/Job.java
@@ -2,16 +2,7 @@
 public interface Job {
 	public void work(); 
 	public default void getMoney(int money) {
-		System.out.println(money + "À» ¹ÞÀ½");
-
-		System.out.println(money + "À» ¹ÞÀ½");
-=======
-		System.out.println("hi");
-		System.out.println("hi");
-		System.out.println("hi");
-		System.out.println("¼öÁ¤ÇÞ´Ù");
-
->>>>>>> master
+		System.out.println("�ѱ��� ������!");
 	}
 
 }
